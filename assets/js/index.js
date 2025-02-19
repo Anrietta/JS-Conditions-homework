bc3ccc7da89ddd5c48e8faef02efcc97e2b15418
Anrietta <sekereshai@gmai.com>
2025-02-20 00:09:11 +0200

    Add solutions to tasks 3-5 and extra task

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,11 +27,11 @@
 
 // 2. Написати функцію, яка приймає сторону квадрата та повертає його площу
 
-const a = Number(prompt('Enter length of the side of the square:'))
-if (!isNaN(a)) {
-    alert(`The area of the square equals ` + Math.pow(a, 2));
-    // console.log(Math.pow(a, 2));
-}
+// const a = Number(prompt('Enter length of the side of the square:'))
+// if (!isNaN(a)) {
+//     alert(`The area of the square equals ` + Math.pow(a, 2));
+//     // console.log(Math.pow(a, 2));
+// }
 
 
 
@@ -47,7 +47,19 @@ if (!isNaN(a)) {
 
 // Задачу вирішити через оператор switch-case
 
+// const menu = Number(prompt('Enter the number of drink: \n 1 - tea \n 2 - coffee \n 3 - juice \n 4 - water \n'));
 
+// if (menu === 1) {
+//     alert('You have chosen tea');
+// } else if (menu === 2) {
+//     alert('You have chosen coffee');
+// } else if (menu === 3) {
+//     alert('You have chosen juice');
+// } else if (menu === 4) {
+//     alert('You have chosen water');
+// } else {
+//     alert('Sorry, you have entered wrong number! Choose one of the offered drinks!');
+// }
 
 
 
@@ -58,14 +70,35 @@ if (!isNaN(a)) {
 // Знижка 3% надається, якщо сума покупки більша за 500 грн., а знижка 5% - якщо сума покупки більша за 800 грн. Використати тернарний оператор
 
 
+// const amount = Number(prompt('Enter the amount of your purschase to get your discount: '))
+// const currency = ' UAH';
+// const discountMin = 3,
+//       discountMax = 5;
 
+// if (amount < 500) {
+//     alert('The amount entered is not enough to get a discount!')
+// } else if (amount >= 500 && amount < 800) {
+//     alert(amount - ((amount*discountMin)/100) + currency);
+// } else if (amount >= 800) {
+//     alert(amount - ((amount*discountMax)/100) + currency);
+// } else {
+//     alert('You entered incorrect value! Please try again!')
+// }
 
 
 
 // 5. Реалізувати функцію, яка приймає два числа і перевіряє, чи ділиться перше на друге націло (без залишку). Функція має повертати булеве значення: true, якщо ділиться без залишку, false - не ділиться без залишку.
 
+// const a = Number(prompt('Enter the first number: ')),
+//       b = Number(prompt('Enter the second number: '));
 
-
+// if (a % b === 0) {
+//     console.log('True');
+//     // console.log(a % b === 0);
+// } else {
+//     console.log('False');
+//     // console.log(a % b === 0);
+// }
 
 
 
@@ -73,3 +106,25 @@ if (!isNaN(a)) {
 // Задачка з * для допитливих: написати функцію, яка приймає три коефіцієнта в якості аргумента та знаходить корені квадратного рівняння
 // (PS. Поки що ми повертати з функції одразу два значення не вмієм, тому хай функція їх просто виведе на консоль або алертом)
 // (гугл - "формула дискримінанта квадратного рівняння")
+
+
+const num1 = +(prompt('Enter the first number') || NaN),    // якщо вводити пусте значення то приведення в числовий тип конвертує пустий стрінг в 0, від цього вся формула розраховується з 0 і потрапляє в умову де discr = 0, а треба щоб пішло в останню умову де значення невіоме, тому тут я додала логічний оператор АБО ||. Тут він повертає перше правдиве значення (саме знанчення а не тру чи фолс), і пустий стрінг який не є числом а є фолсом переводить в NaN, а NaN потравляє в останню умову з помилковими значеннями
+      num2 = +(prompt('Enter the second number') || NaN),
+      num3 = +(prompt('Enter the third number') || NaN);   
+          
+
+const discr = Math.pow(num2, 2) - (4 * num1 * num3);
+
+if (discr > 0) {
+    const x1 = (- num2 + Math.sqrt(discr)) / (2 * num1);
+    const x2 = (- num2 - Math.sqrt(discr)) / (2 * num1);
+    alert ('The roots of this equation are:' + '\n' + x1 + '\n' + x2);
+    console.log(discr);
+} else if (discr === 0) {
+    alert('The root of this equation is ' + discr)
+} else if (discr < 0) {
+    alert('The equation has no real roots.')
+} else {
+    alert ('You entered incorrect numbers! Please try again!')
+    console.log(discr);
+}
